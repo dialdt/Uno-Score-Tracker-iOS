0c0817323e329320d263af882358ea605ede8f8b
Isham Jassat <isham.jassat@gmail.com>
2022-08-21 11:47:16 +0100

    Initial Commit

---
+++ Uno TrackerUITests/Uno_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Uno_TrackerUITestsLaunchTests.swift
+//  Uno TrackerUITests
+//
+//  Created by Isham Jassat on 21/08/2022.
+//
+
+import XCTest
+
+class Uno_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
